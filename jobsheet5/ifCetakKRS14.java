0e63bc2a009466f7e8c623e55f43d4b2a8d53224
Hestia-n <hestiasafitri14@gmail.com>
2025-10-02 10:22:54 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS14.java
+++ jobsheet5/ifCetakKRS14.java
@@ -9,12 +9,16 @@ public class ifCetakKRS14 {
     System.out.println("Apakah UKT sudah lunas? (true/false)");
     boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi.");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    }
-    else {
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
-    }
+    //if (uktLunas) {  ------------ BARIS IF ELSE SEBELUMNYA
+        //System.out.println("Pembayaran UKT terverifikasi.");
+        //System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    //}
+    //else {
+        //System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+    //}
+
+    System.out.println((uktLunas) 
+    ? "KRS ditampilkan" 
+    : "KRS tidak dapat ditampilkan");
     }
 }
